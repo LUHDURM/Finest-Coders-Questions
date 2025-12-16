f02ce75f6e0c0a915a2aef1474386106e2ed34fa
MRUDHUL NR <156283594+LUHDURM@users.noreply.github.com>
2025-12-16 22:41:41 +0530

    Fix indentation in invertTree method

--- Day 8.py
+++ Day 8.py
@@ -5,4 +5,4 @@ class Solution:
         root.left , root.right = root.right , root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
-        return root 
+        return root
